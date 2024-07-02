81296f43e0d0ef6b7e524ee0d74680e4b304826f
SENEOArgb <serezhenkaaverkin@mail.ru>
2024-07-02 16:12:46 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Primer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        int a = 5;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
